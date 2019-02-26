3ba481d303dc4523756a4f3abe796c470a411401
bary.mi <gmrjmm017@gmail.com>
2019-02-26 07:18:41 +0000

    bootstrap-kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     
     def index
-        @tasks = Task.all
+        @tasks = Task.all.page(params[:page])
     end
     
     def show
@@ -52,6 +52,6 @@ class TasksController < ApplicationController
     end
     
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content, :status)
     end
 end
